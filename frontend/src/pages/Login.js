6df59fedadd5f2b99c7b57d235027a36c1957aad
Brito <britoarteiro@gmail.com>
2019-11-16 11:51:51 -0300

    Atualizações começo Front

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../assets/favicon.ico';
+
+
+import './Login.css';
+
+export default function Login() {
+    return (
+        <img src={logo} alt="Ominapp"></img>
+    );
+}
+
